d70ea20f68591fb057f1cdc7a77a5a83efb686dc
Claire Jin (CSI INTERFUSION INC) <v-clairejin@microsoft.com>
2024-06-08 06:04:47 -0400

    last commit

--- renumber.py
+++ renumber.py
@@ -7,6 +7,7 @@ with open("multi-image_eval-data.jsonl", "r") as f:
     for line in f:
         json_line = json.loads(line)
         json_line["question_id"] = qid
+        # json_line["text"] += " In your response, describe each image before answering the question."
         output.append(json_line)
         qid += 1
 
